c957261f2e0c7ca39e4d2bbe7ed4c028127a08f1
Jake Faulkner <jakefaulkn@gmail.com>
2025-07-08 22:44:11 +1200

    add option for polling interval

--- app/Main.hs
+++ app/Main.hs
@@ -10,6 +10,7 @@ import qualified Graphics.Vty as V
 import Graphics.Vty.Config (defaultConfig)
 import Graphics.Vty.CrossPlatform (mkVty)
 import Model.AppState
+import Options.Applicative
 import SQueue.Poller
 import UI.Event (handleEvent)
 import UI.Poller (startPoller)
@@ -49,11 +50,21 @@ appAttrs =
 ------------------------------------------------------------
 -- Main
 
+data Options = Options
+    {pollInterval :: Int}
+
+options :: Parser Options
+options = Options <$> (option auto (long "interval" <> short 'i' <> help "Polling interval for squeue commands. Keep short to keep your admins happy. Does not affect output viewing." <> showDefault <> value 30 <> metavar "TIME (s)"))
+
+cli :: ParserInfo Options
+cli = info (options <**> helper) (fullDesc <> header "slew - Slurm for the rest of us.")
+
 main :: IO ()
 main = do
     eventChannel <- BC.newBChan 10
     commandChannel <- newTChanIO
-    withAsync (squeueThread SQueueStatus eventChannel) $ \_ ->
+    opts <- execParser cli
+    withAsync (squeueThread (pollInterval opts) SQueueStatus eventChannel) $ \_ ->
         withAsync (startPoller commandChannel (BC.writeBChan eventChannel . PollEvent)) $ \_ -> do
             let buildVty = mkVty defaultConfig
                 is = initialState commandChannel
