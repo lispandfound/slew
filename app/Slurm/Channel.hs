e8a82f3ea0f46ab0e422f7b33ad4f8258991bb26
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2025-12-26 14:29:21 +1300

    fix: add missing channel module

---
+++ app/Slurm/Channel.hs
@@ -0,0 +1,82 @@
+module Slurm.Channel where
+
+import Brick.BChan
+import Control.Monad.Trans.Resource
+import Data.Aeson (FromJSON, eitherDecode)
+import Data.ByteString.Lazy (hGetContents)
+import qualified Data.Text.Lazy.Encoding as TLE
+import Model.SlurmCommand (SlurmCommandResult (..), SlurmContext (..), SlurmError (..))
+import Optics.Setter (over)
+import System.Exit (ExitCode (..))
+import System.IO (hClose)
+import System.Process
+
+data SlurmRequest b = SlurmRequest
+    { cp :: CreateProcess
+    , callback :: SlurmCommandResult LByteString -> b
+    }
+
+worker :: BChan (SlurmRequest b) -> BChan b -> IO ()
+worker input output = forever $ do
+    SlurmRequest procSpec cb <- readBChan input
+    -- Extract metadata for our data types
+    let (commandName, commandArgs) = case cmdspec procSpec of
+            ShellCommand s -> (s, [])
+            RawCommand c a -> (c, a)
+
+    result <- runResourceT $ do
+        let pSet = procSpec{std_out = CreatePipe, std_err = CreatePipe}
+        (_, (mOut, mErr, _, ph)) <-
+            allocate
+                (createProcess pSet)
+                ( \(_, hout, herr, ph) -> do
+                    -- Ensure handles are closed and process is reaped
+                    maybe (return ()) hClose hout
+                    maybe (return ()) hClose herr
+                    _ <- waitForProcess ph
+                    return ()
+                )
+        outBytes <- liftIO $ maybe (return mempty) hGetContents mOut
+        errText <- liftIO $ maybe (return mempty) (fmap toText . hGetContents) mErr
+
+        exitStatus <- liftIO $ waitForProcess ph
+
+        case exitStatus of
+            ExitSuccess ->
+                return $
+                    SlurmCommandResult
+                        ( SlurmContext
+                            { cmd = commandName
+                            , args = commandArgs
+                            , stdout = decodeUtf8 outBytes
+                            , stderr = errText
+                            }
+                        )
+                        (Right outBytes)
+            ExitFailure code ->
+                return $
+                    SlurmCommandResult
+                        ( SlurmContext
+                            { cmd = commandName
+                            , args = commandArgs
+                            , stdout = toText outBytes
+                            , stderr = errText
+                            }
+                        )
+                        (Left (ExecutionError code))
+
+    writeBChan output (cb result)
+  where
+    -- Helper to safely decode UTF8 and convert to strict Text
+    toText = toStrict . TLE.decodeUtf8With (\_ _ -> Just ' ')
+
+---
+
+runJson :: (FromJSON a) => BChan (SlurmRequest b) -> CreateProcess -> (SlurmCommandResult a -> b) -> IO ()
+runJson chan process callback = writeBChan chan $ SlurmRequest process (callback . parseJson)
+  where
+    parseJson :: (FromJSON a) => SlurmCommandResult LByteString -> SlurmCommandResult a
+    parseJson = over #result join . fmap (first (DecodingError . toText) . eitherDecode)
+
+runDiscard :: BChan (SlurmRequest a) -> CreateProcess -> (SlurmCommandResult () -> a) -> IO ()
+runDiscard chan process callback = writeBChan chan $ SlurmRequest process (callback . void)
